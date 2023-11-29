ebdfa7ab474690e82e65d0da477317bb92527db0
rekebuba <abukiabdullahi1@gmail.com>
2023-11-29 08:45:47 -0800

    simple_shell

--- main.c
+++ main.c
@@ -5,7 +5,14 @@
  * @argv: argument vector
  * Return: int
  */
-int main (int argc, char **argv)
+
+int main(int argc, char **argv)
 {
-	
+	int a = 9;
+	int b = 8;
+	int  sum = a + b;
+
+	printf("sum %i\n", sum);
+	return (0);
 }
+
