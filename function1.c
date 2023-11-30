e98d6fe9cfd394c9a0398c7c7332968f96f61f35
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-11-30 20:53:18 +0300

    simple_shell

---
+++ function1.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+
+/*
+ * shell_loop - 
+ * 
+ */
+void shell_loop(void)
+{
+	char *line;
+	char **args;
+	int status;
+
+	do
+	{
+		printf("$ ");
+		line = shell_read_line();
+		args = shell_split_line(line);
+		status = shell_execute(args);
+
+		free(line);
+		free(args);
+	} while (status);
+
+	exit(EXIT_FAILURE);
+}
+/**
+ * shell_split_line -
+ *
+ * @line:
+ * Return: char**
+ */
+char **shell_split_line(char *line)
+{
+	int buffer_size = BUFFER;
+	int index = 0;
+	char **tokens = malloc(buffer_size * sizeof(char *));
+	char *token;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "ERROR: failed to allocate memory\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = _strtok(line, TOK_DELIM);
+	while (token != NULL)
+	{
+		tokens[index] = token;
+		index++;
+
+		if (index >= BUFFER)
+		{
+			buffer_size += BUFFER;
+			tokens = realloc(tokens, buffer_size * sizeof(char *));
+			if (!tokens)
+			{
+
+				fprintf(stderr, "ERROR: failed to allocate memory\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = _strtok(NULL, TOK_DELIM);
+	}
+	tokens[index] = NULL;
+	return (tokens);
+}
