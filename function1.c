38d67afb13c8e34d979e7c550e93ba02aedaf2ba
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-12-04 20:56:33 +0300

    simple_shell

--- function1.c
+++ function1.c
@@ -6,61 +6,59 @@
  */
 void shell_loop(void)
 {
-	char *line;
-     	char **args;
-    int status;
+		char *line;
+		char **args;
+		int status;
 
-    do
-    {
-        dprintf(1, "$ ");
-        line = shell_read_line();
-        args = shell_split_line(line);
-        status = shell_launch(args);
+	do {
+		dprintf(1, "$ ");
+		line = shell_read_line();
+		args = shell_split_line(line);
+		status = shell_launch(args);
 
-        free(line);
-        free(args);
-    } while (status);
+		free(line);
+		free(args);
+	} while (status);
 }
 
 /**
  * shell_split_line - it split the line by specifed delimeter
- *
  * @line: the argument passed
  * Return: char**
  */
 char **shell_split_line(char *line)
 {
-    int buffer_size = BUFFER;
-    int index = 0;
-    char **tokens = malloc(buffer_size * sizeof(char *));
-    char *token;
+	int buffer_size = BUFFER;
+	int index = 0;
+	char **tokens = malloc(buffer_size * sizeof(char *));
+	char *token;
 
-    if (!tokens)
-    {
-        perror("ERROR: failed to allocate memory\n");
-        exit(EXIT_FAILURE);
-    }
+	if (!tokens)
+	{
+		perror("ERROR: failed to allocate memory\n");
+		exit(EXIT_FAILURE);
+	}
 
-    token = _strtok(line, TOK_DELIM);
-    while (token != NULL)
-    {
-        tokens[index] = token;
-        index++;
+	token = _strtok(line, TOK_DELIM);
+	while (token != NULL)
+	{
+		tokens[index] = token;
+		index++;
 
-        if (index >= BUFFER)
-        {
-            buffer_size += BUFFER;
-            tokens = realloc(tokens, buffer_size * sizeof(char *));
-            if (!tokens)
-            {
+		if (index >= BUFFER)
+		{
+			buffer_size += BUFFER;
+			tokens = realloc(tokens, buffer_size * sizeof(char *));
+			if (!tokens)
+			{
 
-                perror("ERROR: failed to allocate memory\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-        token = _strtok(NULL, TOK_DELIM);
-    }
-    tokens[index] = NULL;
+				perror("ERROR: failed to allocate memory\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = _strtok(NULL, TOK_DELIM);
+	}
+	tokens[index] = NULL;
 
-    return (tokens);
+	return (tokens);
 }
