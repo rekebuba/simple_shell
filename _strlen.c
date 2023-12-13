e1c7ddd161aa9262c54199e70ae52700e29789cb
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-12-13 20:19:26 +0300

    simple_shell

--- _strlen.c
+++ _strlen.c
@@ -8,11 +8,11 @@
  */
 size_t _strlen(const char *str)
 {
-	const char *s = str;
+const char *s = str;
 
-	while (*s)
-	{
-		s++;
-	}
-	return (s - str);
+while (*s)
+{
+s++;
+}
+return (s - str);
 }
