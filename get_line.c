3b7627136c1907403ec5c264af8d2889d53bc87d
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-12-14 00:47:33 +0300

    simple_shell

--- get_line.c
+++ get_line.c
@@ -9,44 +9,44 @@
  */
 ssize_t get_line(char **line_ptr, size_t *n, FILE *stream)
 {
-    size_t len = 0;
-    char *line = NULL;
-    char *temp;
-    if (line_ptr == NULL || n == NULL)
-        return (-1);
-    while (1)
-    {
-        int c = fgetc(stream);
-        if (c == EOF)
-            break;
-        if (len == 0)
-        {
-            *n = 16;
-            line = malloc(*n);
-            if (line == NULL)
-                return (-1);
-        }
-        line[len++] = c;
-        if (c == '\n')
-            break;
-        if (len == *n)
-        {
-            *n *= 2;
-            temp = realloc(line, *n);
-            if (temp == NULL)
-            {
-                free(line);
-                return (-1);
-            }
-            line = temp;
-        }
-    }
-    if (len == 0)
-    {
-        free(line);
-        return (-1);
-    }
-    line[len] = '\0';
-    *line_ptr = line;
-    return (len);
+	size_t len = 0;
+	char *line = NULL;
+	char *temp;
+	if (line_ptr == NULL || n == NULL)
+		return (-1);
+	while (1)
+	{
+		int c = fgetc(stream);
+		if (c == EOF)
+			break;
+		if (len == 0)
+		{
+			*n = 16;
+			line = malloc(*n);
+			if (line == NULL)
+				return (-1);
+		}
+		line[len++] = c;
+		if (c == '\n')
+			break;
+		if (len == *n)
+		{
+			*n *= 2;
+			temp = realloc(line, *n);
+			if (temp == NULL)
+			{
+				free(line);
+				return (-1);
+			}
+			line = temp;
+		}
+	}
+	if (len == 0)
+	{
+		free(line);
+		return (-1);
+	}
+	line[len] = '\0';
+	*line_ptr = line;
+	return (len);
 }
