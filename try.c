75e5176466ec5b1a9e2964460d0e2895a0f14f51
rekebuba <abukiabdullahi1@gmail.com>
2023-12-13 09:24:26 -0800

    try

---
+++ try.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of a string
+ * @str: the string passed
+ * Return: size_t
+ */
+size_t _strlen(const char *str)
+{
+const char *s = str;
+
+while (*s)
+{
+s++;
+}
+return (s - str);
+}
+
