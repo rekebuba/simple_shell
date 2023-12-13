99925acd22ae0653fff3c95793fd538d91362d05
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-12-13 21:00:01 +0300

    simple_shell

--- _strlen.c
+++ _strlen.c
@@ -2,17 +2,16 @@
 
 /**
  * _strlen - checks the length of a string
- * 
  * @str: the string passed
  * Return: size_t
  */
 size_t _strlen(const char *str)
 {
-const char *s = str;
+	const char *s = str;
 
-while (*s)
-{
-s++;
-}
-return (s - str);
+	while (*s)
+	{
+		s++;
+	}
+	return (s - str);
 }
